1a874f0b1b8078ac9fd6cc54dacdc8a909c64e9a
Oskari <oskari.m.lehtonen@student.lut.fi>
2023-12-07 23:13:10 +0200

    Week 6 initial commit

---
+++ dist/index.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const port = 8000;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.static(path_1.default.join(path_1.default.dirname(__dirname), 'public')));
+let vehicles = [];
+app.get("/hello", (req, res) => {
+    res.send({ "msg": "Hello world" });
+});
+app.post("/vehicle/add", (req, res) => {
+    let newVehicle = req.body;
+    vehicles.push(newVehicle);
+    res.status(201);
+    res.send({ "msg": "Vehicle Added" });
+});
+app.get("/vehicle/search/:model", (req, res) => {
+    let model = req.params.model;
+    let found = false;
+    vehicles.forEach(vehicle => {
+        if (vehicle.model == model) {
+            found = true;
+            res.send(vehicle);
+        }
+    });
+    if (!found) {
+        res.status(404);
+    }
+});
+app.listen(port, () => {
+    console.log("Server running on port:" + port);
+});
---
+++ index.ts
@@ -0,0 +1,60 @@
+import express, {Express, Request, Response} from "express"
+import path from "path"
+const app: Express = express()
+const port: number = 8000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(path.dirname(__dirname), 'public')));
+
+interface Vehicle {
+    model: string,
+    color: string,
+    year: number,
+    power: number
+}
+
+interface Car extends Vehicle {
+    bodytype: string,
+    wheelCount: number
+}
+
+interface Boat extends Vehicle {
+    draft: number
+}
+
+interface Plane extends Vehicle {
+    wingspan: number
+}
+
+let vehicles: Array<Vehicle> = []
+
+app.get("/hello", (req: Request, res: Response) => {
+    res.send({"msg": "Hello world"})
+})
+
+app.post("/vehicle/add", (req: Request, res: Response) => {
+    let newVehicle: Vehicle = req.body
+    vehicles.push(newVehicle)
+    res.status(201)
+    res.send({"msg": "Vehicle Added"})
+})
+
+app.get("/vehicle/search/:model", (req: Request, res: Response) => {
+    let model: string = req.params.model
+    let found: boolean = false
+    vehicles.forEach(vehicle => {
+        if (vehicle.model == model) {
+            found = true
+            res.send(vehicle)
+        }
+    });
+    if (!found) {
+        res.status(404)
+    }
+})
+
+app.listen(port, () => {
+    console.log("Server running on port:" + port)
+
+})
